cbb09166e3126674449b43d074e80358b54cffbf
Jean-Claude <jcsonder@gmail.com>
2017-07-25 07:49:10 +0200

    Start

---
+++ LayoutTest/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace LayoutTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel.StudentViewModel studentViewModelObject = new ViewModel.StudentViewModel();
+            studentViewModelObject.SeedData(10);
+            DataContext = studentViewModelObject;
+        }
+    }
+}
---
+++ LayoutTest/ViewModel/StudentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using LayoutTest.Model;
+
+namespace LayoutTest.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void SeedData(int count)
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            foreach (int n in Enumerable.Range(1, count))
+            {
+                students.Add(new Student { FirstName = new string('a', n), LastName = new string('b', n + n) });
+            }
+
+            Students = students;
+        }
+    }
+}
